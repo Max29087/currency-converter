e95ba3e210bc549b047e824c6daa968b0016b13a
Max29087 <topusorker087@gmai.com>
2025-07-26 21:47:47 +0600

    Add

---
+++ currency_converter.js
@@ -0,0 +1,90 @@
+const dropdowns = document.querySelectorAll(".dropdown select");
+const btn = document.querySelector("form button");
+const fromCurr = document.querySelector(".from select");
+const toCurr = document.querySelector(".to select");
+
+// Currency options populate
+for (let select of dropdowns) {
+    for (code in countryList) {
+        let newOption = document.createElement("option");
+        newOption.innerText = code;
+        newOption.value = code;
+        if (select.name === "from" && code === "USD") {
+            newOption.selected = "selected";
+        } else if (select.name === "to" && code === "BDT") {
+            newOption.selected = "selected";
+        }
+        select.append(newOption);
+    }
+    select.addEventListener("change", (evt) => {
+        updateFlag(evt.target);
+    });
+}
+
+// Update flag image when currency changes
+const updateFlag = (element) => {
+    let currCode = element.value;
+    let countryCode = countryList[currCode];
+    let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+    let img = element.parentElement.querySelector("img");
+    img.src = newSrc;
+};
+
+// // Main Exchange Rate button click
+// btn.addEventListener("click", async (evt) => {
+//     evt.preventDefault();
+//     let amount = document.querySelector(".amount input");
+//     let amountVal = amount.value;
+
+//     if (amountVal === "" || amountVal <= 0) {
+//         amountVal = 1;
+//         amount.value = 1;
+//     }
+
+//     const URL = `https://api.exchangerate.host/convert?from=${fromCurr.value}&to=${toCurr.value}&amount=${amountVal}`;
+
+//     try {
+//         let response = await fetch(URL);
+//         let data = await response.json();
+
+//         if (data && data.result !== undefined) {
+//             let convertedAmount = data.result.toFixed(2);
+//             document.querySelector(".msg").innerText =
+//                 `${amountVal} ${fromCurr.value} = ${convertedAmount} ${toCurr.value}`;
+//         } else {
+//             document.querySelector(".msg").innerText = "Something went wrong. 😢";
+//         }
+//     } catch (error) {
+//         console.error("Error fetching API:", error);
+//         document.querySelector(".msg").innerText = "Something went wrong. 😢";
+//     }
+// });
+btn.addEventListener("click", async (evt) => {
+    evt.preventDefault();
+    let amount = document.querySelector(".amount input");
+    let amountVal = amount.value;
+
+    if (amountVal === "" || amountVal <= 0) {
+        amountVal = 1;
+        amount.value = 1;
+    }
+
+    const URL = `https://api.exchangerate.host/convert?from=${fromCurr.value}&to=${toCurr.value}&amount=${amountVal}`;
+
+    try {
+        let response = await fetch(URL);
+        let data = await response.json();
+        console.log("API response:", data);
+
+        if (data && data.result !== undefined) {
+            let convertedAmount = data.result.toFixed(2);
+            document.querySelector(".msg").innerText =
+                `${amountVal} ${fromCurr.value} = ${convertedAmount} ${toCurr.value}`;
+        } else {
+            document.querySelector(".msg").innerText = "Something went wrong. 😢";
+        }
+    } catch (error) {
+        console.error("Fetch error:", error);
+        document.querySelector(".msg").innerText = "Something went wrong. 😢";
+    }
+});
